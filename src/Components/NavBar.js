658db3c0e343effc53caf21c47de081d50921dcc
rosariov21 <rosariovelazquez@Rosarios-MBP.home>
2020-08-26 16:38:51 -0400

    push

---
+++ src/Components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import App from '../App.css'
+import Home from './Home'
+import InterviewDirectory from './InterviewDirectory'
+
+
+
+import { BrowserRouter as Router,
+Switch,
+Link,
+Route
+} from "react-router-dom"
+
+class NavBar extends React.Component{
+    render(){
+        return(
+            <Router>
+                <div className="nav_par">
+                
+                        <ul className = "nav_ul">
+                        <li className="list_item"><Link to = '/Home'>Home</Link></li>
+                        <li className="list_item"><Link to = '/interviewDirectory'>Interview Directory</Link></li>
+                        </ul>
+                  
+                    <Switch>
+                        <Route exact path = '/Home' component={Home}/>
+                        <Route path ='/interviewDirectory' component={InterviewDirectory}/>
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
+
+}
+export default NavBar
